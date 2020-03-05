3a368b08f5cb9f3d9b76167c22eac6df7fef6857
joao-avelino-92 <joaoavnt@gmail.com>
2020-03-05 11:39:03 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    title:
+      "Black Dog",
+    artist: "Led Zeppelin",
+    cover: "zeppelin.png",
+    file: "black-dog.mp3"
+  },
+  {
+    title: "Time",
+    artist: "Pink Floyd",
+    cover: "pink-floyd.jpeg",
+    file: "time.mp3"
+  },
+  {
+    title: "Tom Sawyer",
+    artist: "Rush",
+    cover: "rush.jpg",
+    file: "tom-sawyer.mp3"
+  }
+];
